3998061b26eb6dad4178f37e20ec8cf7b7852413
muhammad.izzat.2.fadzlon@continental-corporation.com <muhammad.izzat.2.fadzlon@continental-corporation.com>
2023-11-28 00:38:49 +0800

    completed assignment, 28/11

--- assignment/demo.test.js
+++ assignment/demo.test.js
@@ -1,14 +1,19 @@
+//demo.test.js is the Test Suite
+
 const {studentList, printStudentNames} = require("./q2-arrays-loop");
 const {convert} = require("./q3-objects");
 
-describe("Demo", ()=>{
+describe("Demo", ()=>{  //Test 1
 
     it("should test the printStudentNames function of q2", ()=>{
         const spyFn = jest.spyOn(console, "log");
         printStudentNames();
         expect(spyFn).toHaveBeenCalledTimes(studentList.length);
     })
+})
 
+
+describe("Demo2", ()=>{ //Test 2
     it("should test the convert function of q3", ()=>{
         const keyArr = ["NLB1", "NLB2"];
         const valueArr = ["Book A", "Book B"];
